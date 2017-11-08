55034853b532516a2f3fbe31c5011a8152a80773
anthony puitiza <anthony.puitiza@unmsm.edu.pe>
2017-11-08 12:17:17 -0500

    Solucione el problema de poner atrás en el fragment
    listo

--- app/src/main/java/com/robpercival/maplocationdemo/Activity/Container/ContainerCocheraActivity.java
+++ app/src/main/java/com/robpercival/maplocationdemo/Activity/Container/ContainerCocheraActivity.java
@@ -13,8 +13,6 @@ import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerCocheraActivity extends AppCompatActivity {
-    CocheraFragment cocheraFrag;
-    ServiciosFragment serviciosFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +29,25 @@ public class ContainerCocheraActivity extends AppCompatActivity {
             public void onTabSelected(@IdRes int tabId) {
                 switch (tabId){
                     case R.id.detalleCochera:
-                        CocheraFragment cocheraFragment = new CocheraFragment();
-                        cocheraFragment.setCochera(cochera);
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_Cocheras, cocheraFragment)
-                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                                .addToBackStack(null).commit();
+                            CocheraFragment cocheraFragment = new CocheraFragment();
+                            cocheraFragment.setCochera(cochera);
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container_Cocheras, cocheraFragment)
+                                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                    .commit();
                         break;
                     case R.id.detalleServicios:
                         ServiciosFragment serviciosFragment = new ServiciosFragment();
                         serviciosFragment.setCochera(cochera);
                         getSupportFragmentManager().beginTransaction().replace(R.id.container_Cocheras, serviciosFragment)
                                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                                .addToBackStack(null).commit();
+                                .commit();
                         break;
 
+                   /* getSupportFragmentManager().beginTransaction().replace(R.id.container_Cocheras, serviciosFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .addToBackStack(null).commit();*/
+                    /*Don't call ft.addToBackStack(null) when you add the Fragment in onCreate.
+                    That tells the FragmentManger that you have another state BEFORE that fragment that you want to be able to jump back to.*/
                 }
             }
         });
